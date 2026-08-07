025dfa6c18e143759d68b68cb21e00ecd3ece411
ismail2234 <atlam46@gmail.com>
2026-08-07 10:52:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 import time
 import requests
 import pandas as pd

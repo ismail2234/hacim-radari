e8340d3d889eb4e13c4464f603ab8c28e8d03cbb
ismail2234 <atlam46@gmail.com>
2026-08-07 06:11:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from threading import Thread
 
 # ================= AYARLAR =================
 
-TELEGRAM_BOT_TOKEN = "8740764565:AAFwW-VRxTQQ_K0XFHtlwFteYGbefV0sjJM,"
+TELEGRAM_BOT_TOKEN = "8740764565:AAFwW-VRxTQQ_K0XFHtlwFteYGbefV0sjJM"
 TELEGRAM_CHAT_ID = "937967050"
 
 MIN_VOLUME_USD = 15000

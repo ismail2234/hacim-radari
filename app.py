d5a00a1f3c9b7de5c97fd45cd3d405452ac90d6a
ismail2234 <atlam46@gmail.com>
2026-08-07 23:50:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
-
 import os
-import time
 import requests
 from flask import Flask
 from threading import Thread
+from scanner import start_scanner
+
 
 app = Flask(__name__)
 
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+TELEGRAM_BOT_TOKEN = os.getenv(
+    "TELEGRAM_BOT_TOKEN"
+)
+
+TELEGRAM_CHAT_ID = os.getenv(
+    "TELEGRAM_CHAT_ID"
+)
+
 
 
 @app.route("/")
@@ -16,43 +23,53 @@ def home():
     return "🐋 Balina Radar PRO Aktif"
 
 
+
 def send_telegram(message):
-    if not TELEGRAM_BOT_TOKEN or not TELEGRAM_CHAT_ID:
+
+    if not TELEGRAM_BOT_TOKEN:
         return
 
-    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    url = (
+        f"https://api.telegram.org/"
+        f"bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    )
+
 
     data = {
         "chat_id": TELEGRAM_CHAT_ID,
         "text": message
     }
 
+
     try:
-        requests.post(url, json=data, timeout=10)
+        requests.post(
+            url,
+            json=data,
+            timeout=10
+        )
+
     except Exception as e:
-        print(e)
+        print(
+            "Telegram hata:",
+            e
+        )
 
-
-def scanner():
-    while True:
-        print("Radar taraması çalışıyor...")
-        
-        # Buraya analiz motorlarını ekleyeceğiz:
-        # RSI
-        # MACD
-        # EMA
-        # Hacim
-        # Balina analizi
-
-        time.sleep(300)
 
 
 def run_server():
-    port = int(os.environ.get("PORT", 8080))
+
+    port = int(
+        os.environ.get(
+            "PORT",
+            8080
+        )
+    )
+
     app.run(
         host="0.0.0.0",
         port=port
     )
+
 
 
 if __name__ == "__main__":
@@ -62,4 +79,5 @@ if __name__ == "__main__":
         daemon=True
     ).start()
 
-    scanner()
+
+    start_scanner()

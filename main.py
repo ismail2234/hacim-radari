dd4a6e04c0497e97bb3fbc8f4858d873cd813d0d
ismail2234 <atlam46@gmail.com>
2026-08-11 21:10:13 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+{
+  "message": "SyntaxError: expected 'except' or 'finally' block",
+  "severity": "error",
+  "attributes": {
+    "level": "error"
+  },
+  "tags": {
+    "project": "06d12be8-d6fa-4665-936c-6cbfa32d9775",
+    "environment": "dc13f27c-2297-448e-a210-c32d4fbb41e1",
+    "service": "1906fd7e-eda6-44d0-bd37-ecbed0b00d3f",
+    "deployment": "283119fe-ccb8-4cfb-857f-34fa0af08fd3",
+    "replica": "17a4e7c8-6599-4fca-a7cc-b11b22913f47"
+  },
+  "timestamp": "2026-08-11T18:02:51.731765874Z"
+}

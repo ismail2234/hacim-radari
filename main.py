59b2dbb5c5aee873354f6126035cff8d761d83c5
ismail2234 <atlam46@gmail.com>
2026-08-19 23:19:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ def candidates(data):
         key=lambda x: x["volume"] * (1 + max(x["chg"], 0) / 100),
         reverse=True,
     )
-    return result[:SETTINGS.shortlist_size]
+    rreturn result
 
 
 def analyze_one(item):
